9a73c61b9f367d1e4d1871c0b0b1a26b75520da7
Adam Peacock <adam@thepeacock.net>
2021-07-18 21:36:35 -0400

    Change color calendar to Rectory colors

--- calendar_generator/pdf_presets.py
+++ calendar_generator/pdf_presets.py
@@ -4,7 +4,7 @@ from reportlab.lib import colors
 
 from . import pdf
 
-color_accent = colors.indigo
+color_accent = colors.Color(219/255.0, 119/255.0, 52/255.0)
 
 black_and_white = pdf.CalendarStyle(
     outline_color=colors.black,
@@ -19,8 +19,8 @@ black_and_white = pdf.CalendarStyle(
 )
 
 color = pdf.CalendarStyle(
-    outline_color=colors.black,
-    grid_line_color=colors.black,
+    outline_color=color_accent,
+    grid_line_color=color_accent,
     title_color=color_accent,
 
     header_background_color=color_accent,
