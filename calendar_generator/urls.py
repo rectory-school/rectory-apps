ba2246ad5fa50ea6637985dd66fc8b3566f82ce1
Adam Peacock <adam@thepeacock.net>
2021-07-18 21:35:33 -0400

    Link changes

--- calendar_generator/urls.py
+++ calendar_generator/urls.py
@@ -9,11 +9,8 @@ app_name = 'calendar_generator'
 urlpatterns = [
     path('', views.Calendars.as_view(), name='calendar-list'),
     path('<int:pk>/', views.Calendar.as_view(), name='calendar'),
-    path('<int:calendar_id>/pdf/<int:year>/<int:month>/', views.PDFMonth.as_view(), name='month-pdf'),
+
     path('<int:calendar_id>/pdf/<str:preset_slug>/<int:year>/<int:month>/', views.PDFMonth.as_view(), name='month-pdf'),
-
-    path('<int:calendar_id>/pdf/all_months/', views.PDFMonths.as_view(), name='months-pdf'),
     path('<int:calendar_id>/pdf/<str:preset_slug>/all_months/', views.PDFMonths.as_view(), name='months-pdf'),
-
     path('<int:calendar_id>/pdf/<str:preset_slug>/one-page/', views.PDFOnePage.as_view(), name='one-page-pdf'),
 ]
