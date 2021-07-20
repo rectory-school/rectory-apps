67b4b6a2059fef0e3cf7d0b9316ba038f982af05
Adam Peacock <adam@thepeacock.net>
2021-07-20 11:50:01 -0400

    Allow user creation without a password

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,13 +3,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import gettext_lazy as _
+from django import forms
 
 from .models import User
+
+
+class AddUserFormWithoutPassword(forms.ModelForm):
+    """Add a user without a password"""
+
+    # This really just overrides clean so it isn't checking for passwords.
+    # This is mainly because users of the app are expected to log in via Google,
+    # which will correlate to the existing user and log in. Creating the user
+    # before hand allows them to be assigned permissions, as the Google sign-in
+    # process will create a user without a password anyway
+
+    model = User
 
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
     """Admin user model"""
+
+    add_form = AddUserFormWithoutPassword
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
@@ -21,9 +36,10 @@ class UserAdmin(DjangoUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
+            'fields': ('email', 'first_name', 'last_name'),
         }),
     )
+
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
