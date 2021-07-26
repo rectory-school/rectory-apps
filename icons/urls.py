b20d21daf2a69b3460db9d8c24be863a54fbc750
Anonymous <anonymous@example.com>
2021-07-25 23:32:59 -0400

    Add sparse views

--- icons/urls.py
+++ icons/urls.py
@@ -7,7 +7,11 @@ from . import views
 #  pylint: disable=invalid-name
 app_name = 'icons'
 urlpatterns = [
-    path('', views.PageList.as_view(), name='page-list'),
+    path('', views.PageList.as_view(),
+         name='page-list'),
     path('set_positions/', views.set_page_positions, name="set-sort-positions"),
-    path('web/<slug:slug>/', views.PageDetail.as_view(), name="page"),
-]
+    path('web/<slug:slug>/', views.PageDetail.as_view(),
+         name="page"),
+    path(
+        'web/<slug:slug>/sparse/', views.PageDetail.as_view(template_name="icons/page_detail_sparse.html"),
+        name="page-sparse"), ]
