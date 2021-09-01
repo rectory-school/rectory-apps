cfac04041ed4a88e003348c9c4ff3cac6f9779dd
Adam Peacock <adam@thepeacock.net>
2021-09-01 13:54:04 -0400

    View only admin

--- sis/admin.py
+++ sis/admin.py
@@ -2,3 +2,21 @@
 
 from django.contrib import admin
 
+
+class ViewOnlyAdmin(admin.ModelAdmin):
+    """Admin view for mixin that provides view-only access"""
+
+    def has_add_permission(self, request) -> bool:
+        del request
+
+        return False
+
+    def has_delete_permission(self, request, obj=None) -> bool:
+        del request, obj
+
+        return False
+
+    def has_change_permission(self, request, obj=None) -> bool:
+        del request, obj
+
+        return False
