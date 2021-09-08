dde85b4755a45a689ee666653a046435a5f05fe5
Adam Peacock <adam@thepeacock.net>
2021-09-08 14:03:33 -0400

    Logs (#46)
    * Basic remote logging
    
    * Better logging
    
    * Added logging middleware
    
    * Logging config updates
    
    Don't disable existing loggers
    Disable debug on logz
    
    * Better logging
    
    * Don't redefine uvicorn

---
+++ logging_middleware/middleware.py
@@ -0,0 +1,42 @@
+"""Logging middleware"""
+
+import logging
+from django.utils import timezone
+from ipware import get_client_ip
+
+
+class LoggingMiddleware:
+    """Middleware that logs all requests/responses"""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.log = logging.getLogger('log-http-requests')
+        self.log.info("Logging middleware inititalized")
+
+    def __call__(self, request):
+        # Pre-request
+        started_at = timezone.now()
+        client_ip, _ = get_client_ip(request)
+
+        response = self.get_response(request)
+        response_size = len(response.content)
+
+        # Post-request
+        finished_at = timezone.now()
+
+        took = finished_at - started_at
+
+        extra = {
+            'remote-host': client_ip,
+            'request-time': took.total_seconds(),
+            'path': request.path,
+            'method': request.method,
+            'response-size': response_size,
+            'status-code': response.status_code,
+        }
+
+        if hasattr(request, 'user'):
+            extra['user'] = str(request.user)
+
+        self.log.info("%s %s %d", request.method, request.path, response.status_code, extra=extra)
+        return response
