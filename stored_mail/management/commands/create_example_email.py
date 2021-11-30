1233f5a5885de9b109317c4c456a6bcb69548a51
Adam Peacock <adam@thepeacock.net>
2021-11-30 12:12:55 -0500

    Create multiple sample emails

--- stored_mail/management/commands/create_example_email.py
+++ stored_mail/management/commands/create_example_email.py
@@ -15,25 +15,28 @@ class Command(BaseCommand):
     def add_arguments(self, parser) -> None:
         parser.add_argument('from_address')
         parser.add_argument('to_address')
+        parser.add_argument('--count', type=int, default=1)
 
     def handle(self, *args, **options):
         from_name, from_address = email.utils.parseaddr(options['from_address'])
         to_name, to_address = email.utils.parseaddr(options['to_address'])
+        count = options['count']
 
         now = timezone.now()
 
         with transaction.atomic():
-            outgoing = models.OutgoingMessage()
-            outgoing.from_address = from_address
-            outgoing.from_name = from_name
-            outgoing.subject = "Test message"
-            outgoing.text = f"This is a test message generated at {now}"
-            outgoing.html = f"<body><div>This is a test message generated at {now}</div></body>"
-            outgoing.save()
+            for i in range(count):
+                outgoing = models.OutgoingMessage()
+                outgoing.from_address = from_address
+                outgoing.from_name = from_name
+                outgoing.subject = "Test message"
+                outgoing.text = f"This is a test message generated at {now}, sequence {i+1} of {count}"
+                outgoing.html = f"<body><div>This is a test message generated at {now}, sequence {i+1} of {count}</div></body>"
+                outgoing.save()
 
-            send_addr = models.RelatedAddress()
-            send_addr.message = outgoing
-            send_addr.field = 'to'
-            send_addr.name = to_name
-            send_addr.address = to_address
-            send_addr.save()
+                send_addr = models.RelatedAddress()
+                send_addr.message = outgoing
+                send_addr.field = 'to'
+                send_addr.name = to_name
+                send_addr.address = to_address
+                send_addr.save()
