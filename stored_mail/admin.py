e24c0fa8bad8a989dcdcd5f37e0a25a4ffae3c7d
Adam Peacock <adam@thepeacock.net>
2022-05-27 11:13:55 -0400

    Admin filtering for stored mail

--- stored_mail/admin.py
+++ stored_mail/admin.py
@@ -1,6 +1,8 @@
 """Admin for stored email"""
 
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 from . import models
 
@@ -36,15 +38,40 @@ class RelatedAddressInline(admin.TabularInline):
     model = models.RelatedAddress
 
 
+class OutgoingMailAdminSentStatus(admin.SimpleListFilter):
+    """Basic filter for outgoing mail status"""
+
+    title = _('status')
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('sent', _('Sent')),
+            ('unsent', _('Unsent')),
+            ('discarded', _('Discarded')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'sent':
+            return queryset.filter(sent_at__isnull=False)
+
+        if self.value() == 'discarded':
+            return queryset.filter(sent_at__isnull=True, discard_after__lte=timezone.now())
+
+        if self.value() == 'unsent':
+            return queryset.filter(sent_at__isnull=True, discard_after__gt=timezone.now())
+
+
 @admin.register(models.OutgoingMessage)
 class OutgoingMailAdmin(ViewOnlyAdminMixin, admin.ModelAdmin):
     """Admin for outgoing mail"""
 
     inlines = [RelatedAddressInline]
-    list_filter = ['sent_at', 'created_at', 'last_send_attempt']
+    list_filter = [OutgoingMailAdminSentStatus, 'sent_at', 'created_at']
     list_display = ['pk', 'subject', 'created_at', 'sent_at', 'last_send_attempt']
 
-    fields = ['from_name', 'from_address', 'subject', 'sent_at', 'created_at', 'encoded']
+    fields = ['from_name', 'from_address', 'subject', 'created_at',
+              'discard_after', 'last_send_attempt', 'sent_at', 'encoded']
     readonly_fields = ['encoded']
 
     @admin.display(description='Encoded Message')
