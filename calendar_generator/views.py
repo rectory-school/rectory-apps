0e55f79f17584398338de0bce1d3b50255ea2647
Adam Peacock <adam@thepeacock.net>
2022-05-30 10:18:46 -0400

    Variable rename

--- calendar_generator/views.py
+++ calendar_generator/views.py
@@ -73,12 +73,12 @@ class Calendar(CalendarViewPermissionRequired, DetailView):
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        object: models.Calendar = self.get_object()
+        cal_obj: models.Calendar = self.get_object()
 
         months = set()
 
-        days_dict = object.get_date_letter_map()
-        labels_dict = object.get_arbitrary_labels()
+        days_dict = cal_obj.get_date_letter_map()
+        labels_dict = cal_obj.get_arbitrary_labels()
 
         for date_key in days_dict:
             months.add((date_key.year, date_key.month))
@@ -116,17 +116,17 @@ class Calendar(CalendarViewPermissionRequired, DetailView):
         if today in days_dict:
             context["today_letter"] = days_dict[today]
 
-        context["day_rotation"] = [d.letter for d in object.days.all()]
+        context["day_rotation"] = [d.letter for d in cal_obj.days.all()]
         context["skipped_days"] = sorted(
-            (s.date, s.end_date) for s in object.skips.all()
+            (s.date, s.end_date) for s in cal_obj.skips.all()
         )
         context["reset_days"] = [
             (obj.date, obj.day.letter)
-            for obj in object.reset_days.select_related("day").all()
+            for obj in cal_obj.reset_days.select_related("day").all()
         ]
 
         context["calendars"] = month_grids
-        context["custom_calendar_form"] = forms.CustomCalendarForm(calendar=object)
+        context["custom_calendar_form"] = forms.CustomCalendarForm(calendar=cal_obj)
 
         return context
 
