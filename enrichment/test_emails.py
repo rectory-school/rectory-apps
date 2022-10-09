edd5e3f04cc2c8ec0196c8d3aa78e0b8e68c26a6
Adam Peacock <adam@thepeacock.net>
2022-10-08 22:27:04 -0400

    Add tests for all email configs

--- enrichment/test_emails.py
+++ enrichment/test_emails.py
@@ -9,9 +9,135 @@ import pytest
 from django.conf import settings
 
 import blackbaud.models
-import enrichment.models
 
-from enrichment.emails import unassigned_advisor, OutgoingEmail, AddressPair
+from enrichment.emails import (
+    get_outgoing_messages,
+    unassigned_advisor,
+    OutgoingEmail,
+    AddressPair,
+)
+
+from enrichment.models import (
+    EmailConfig,
+    RelatedAddress,
+    Option,
+    Slot,
+    EMAIL_REPORT_CHOICES,
+)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("report_name", (choice[0] for choice in EMAIL_REPORT_CHOICES))
+def test_generate_email(report_name: str):
+    middle_school = blackbaud.models.School(
+        sis_id=uuid4().hex,
+        active=True,
+        name="Middle School",
+    )
+    middle_school.save()
+
+    course = blackbaud.models.Course(
+        sis_id=uuid4().hex,
+        active=True,
+        title="Advisory",
+    )
+    course.save()
+
+    student = blackbaud.models.Student(
+        sis_id=uuid4().hex,
+        active=True,
+        given_name="Jimmy",
+        family_name="Neutron",
+        email="example@example.org",
+    )
+    student.save()
+    student.schools.add(middle_school)
+
+    teacher = blackbaud.models.Teacher(
+        sis_id=uuid4().hex,
+        active=True,
+        given_name="Adam",
+        family_name="Peacock",
+        email="example@example.org",
+    )
+    teacher.save()
+
+    section = blackbaud.models.Class(
+        sis_id=uuid4().hex,
+        active=True,
+        title="Peacock advising",
+        course=course,
+        school=middle_school,
+    )
+    section.save()
+
+    blackbaud.models.TeacherEnrollment.objects.create(
+        sis_id=uuid4().hex,
+        active=True,
+        section=section,
+        teacher=teacher,
+        school=middle_school,
+        begin_date=date.today() - timedelta(days=30),
+        end_date=date.today() + timedelta(days=30),
+    )
+
+    blackbaud.models.StudentEnrollment.objects.create(
+        sis_id=uuid4().hex,
+        active=True,
+        section=section,
+        student=student,
+        school=middle_school,
+        begin_date=date.today() - timedelta(days=30),
+        end_date=date.today() + timedelta(days=30),
+    )
+
+    blackbaud.models.AdvisoryCourse.objects.create(course=course)
+    blackbaud.models.AdvisorySchool.objects.create(school=middle_school)
+
+    slot = Slot()
+    slot.date = date(2022, 10, 11)  # Tuesday
+    slot.editable_until = datetime(
+        slot.date.year,
+        slot.date.month,
+        slot.date.day,
+        12,
+        0,
+        0,
+        0,
+        ZoneInfo("America/New_York"),
+    )
+    slot.save()
+
+    option = Option()
+    option.start_date = date.today() - timedelta(days=7)
+    option.end_date = option.start_date + timedelta(days=365)
+
+    option.teacher = teacher
+    option.save()
+
+    cfg = EmailConfig()
+    cfg.report = report_name
+    cfg.start = 0
+    cfg.end = 0
+    cfg.time = time(11, 0, 0)
+    cfg.monday = True
+    cfg.tuesday = True
+    cfg.wednesday = True
+    cfg.thursday = True
+    cfg.friday = True
+    cfg.saturday = True
+    cfg.sunday = True
+
+    cfg.from_name = "Rectory Enrichment System"
+    cfg.from_address = "server@apps.rectoryschool.org"
+
+    cfg.save()
+
+    msgs = get_outgoing_messages(cfg, slot.date)
+
+    for msg in msgs:
+        assert msg.message_html
+        assert msg.message_text
 
 
 @pytest.mark.django_db
@@ -20,7 +146,7 @@ def test_advisor_unassigned():
 
     teacher, student, slot = _basic_setup()
 
-    cfg = enrichment.models.EmailConfig()
+    cfg = EmailConfig()
     cfg.report = "unassigned_advisor"
     cfg.start = 0
     cfg.end = 6
@@ -37,7 +163,7 @@ def test_advisor_unassigned():
 
     cfg.save()
 
-    enrichment.models.RelatedAddress.objects.create(
+    RelatedAddress.objects.create(
         name="Lisa",
         address="admin-user@rectoryschool.org",
         field="reply-to",
@@ -144,7 +270,7 @@ def _basic_setup():
     blackbaud.models.AdvisoryCourse.objects.create(course=course)
     blackbaud.models.AdvisorySchool.objects.create(school=middle_school)
 
-    slot = enrichment.models.Slot()
+    slot = Slot()
     slot.date = date(2022, 10, 11)  # Tuesday
     slot.editable_until = datetime(
         slot.date.year,
@@ -158,7 +284,7 @@ def _basic_setup():
     )
     slot.save()
 
-    option = enrichment.models.Option()
+    option = Option()
     option.start_date = date.today() - timedelta(days=7)
     option.end_date = option.start_date + timedelta(days=365)
 
