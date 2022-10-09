1b88d8aa593d5419793ef066b12d681843d6ac41
Adam Peacock <adam@thepeacock.net>
2022-10-09 00:59:52 -0400

    Split up email tests

--- enrichment/test_emails.py
+++ enrichment/test_emails.py
@@ -28,7 +28,7 @@ from enrichment.models import (
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("report_name", (choice[0] for choice in EMAIL_REPORT_CHOICES))
-def test_generate_email(report_name: str):
+def test_generate_email_html(report_name: str):
     middle_school = blackbaud.models.School(
         sis_id=uuid4().hex,
         active=True,
@@ -137,6 +137,118 @@ def test_generate_email(report_name: str):
 
     for msg in msgs:
         assert msg.message_html
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("report_name", (choice[0] for choice in EMAIL_REPORT_CHOICES))
+def test_generate_email_text(report_name: str):
+    middle_school = blackbaud.models.School(
+        sis_id=uuid4().hex,
+        active=True,
+        name="Middle School",
+    )
+    middle_school.save()
+
+    course = blackbaud.models.Course(
+        sis_id=uuid4().hex,
+        active=True,
+        title="Advisory",
+    )
+    course.save()
+
+    student = blackbaud.models.Student(
+        sis_id=uuid4().hex,
+        active=True,
+        given_name="Jimmy",
+        family_name="Neutron",
+        email="example@example.org",
+    )
+    student.save()
+    student.schools.add(middle_school)
+
+    teacher = blackbaud.models.Teacher(
+        sis_id=uuid4().hex,
+        active=True,
+        given_name="Adam",
+        family_name="Peacock",
+        email="example@example.org",
+    )
+    teacher.save()
+
+    section = blackbaud.models.Class(
+        sis_id=uuid4().hex,
+        active=True,
+        title="Peacock advising",
+        course=course,
+        school=middle_school,
+    )
+    section.save()
+
+    blackbaud.models.TeacherEnrollment.objects.create(
+        sis_id=uuid4().hex,
+        active=True,
+        section=section,
+        teacher=teacher,
+        school=middle_school,
+        begin_date=date.today() - timedelta(days=30),
+        end_date=date.today() + timedelta(days=30),
+    )
+
+    blackbaud.models.StudentEnrollment.objects.create(
+        sis_id=uuid4().hex,
+        active=True,
+        section=section,
+        student=student,
+        school=middle_school,
+        begin_date=date.today() - timedelta(days=30),
+        end_date=date.today() + timedelta(days=30),
+    )
+
+    blackbaud.models.AdvisoryCourse.objects.create(course=course)
+    blackbaud.models.AdvisorySchool.objects.create(school=middle_school)
+
+    slot = Slot()
+    slot.date = date(2022, 10, 11)  # Tuesday
+    slot.editable_until = datetime(
+        slot.date.year,
+        slot.date.month,
+        slot.date.day,
+        12,
+        0,
+        0,
+        0,
+        ZoneInfo("America/New_York"),
+    )
+    slot.save()
+
+    option = Option()
+    option.start_date = date.today() - timedelta(days=7)
+    option.end_date = option.start_date + timedelta(days=365)
+
+    option.teacher = teacher
+    option.save()
+
+    cfg = EmailConfig()
+    cfg.report = report_name
+    cfg.start = 0
+    cfg.end = 0
+    cfg.time = time(11, 0, 0)
+    cfg.monday = True
+    cfg.tuesday = True
+    cfg.wednesday = True
+    cfg.thursday = True
+    cfg.friday = True
+    cfg.saturday = True
+    cfg.sunday = True
+
+    cfg.from_name = "Rectory Enrichment System"
+    cfg.from_address = "server@apps.rectoryschool.org"
+
+    cfg.save()
+
+    msgs = get_outgoing_messages(cfg, slot.date)
+
+    for msg in msgs:
         assert msg.message_text
 
 
